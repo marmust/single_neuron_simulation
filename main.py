992edef1959700534f8b3d7078189be2402c6401
marmust <daniel.varkin@gmail.com>
2022-02-11 22:46:11 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# PLEASE LOOK AT EXPLANATION FILE
+
+# SINGLE NEURON
+input1 = float(input("in1: "))
+input2 = float(input("in2: "))
+weight = 1
+
+avarage = (input1 + input2) / 2
+neuronOut = avarage * weight
+
+print(neuronOut)
+# SINGLE NEURON
